c4bc7b5cff8ece8385e1e197754a0dbaf229abd4
Manieswar2002 <saimani9381787332@gmail.com>
2024-12-15 10:56:56 +0530

    v1

---
+++ fooditem.js
@@ -0,0 +1,404 @@
+const foodItem= [
+    {
+    id: 1,
+    name: 'Coffee latte',
+    category : 'Coffee',
+    rating : 4.3,
+    price: 130,
+    img: 'Coffee latte.jpeg',
+    quantity: 1
+},
+{
+    id: 2,
+    name: 'Cappuccino',
+    category : 'Coffee',
+    rating : 4.3,
+    price: 150,
+    img: 'cappuccino.jpg',
+    quantity: 1
+},
+{
+    id: 3,
+    name: 'Flat white',
+    category : 'Coffee',
+    rating : 4.3,
+    price: 180,
+    img: ' flatwhite.jpg',
+    quantity: 1
+},
+{
+    id: 4,
+    name: 'Americano',
+    category : 'Coffee',
+    rating : 4.3,
+    price: 156,
+    img: 'Americano.jpg',
+    quantity: 1
+},
+{
+    id: 5,
+    name: 'Vanilla latte',
+    category : 'Coffee',
+    rating : 4.3,
+    price: 169,
+    img: 'velvet.jpg',
+    quantity: 1
+},
+{
+    id: 6,
+    name: 'Choco cappuccino',
+    category : 'Coffee',
+    rating : 4.3,
+    price: 209,
+    img: 'Chocolate cappuccino.jpg',
+    quantity: 1
+},
+{
+    id: 7,
+    name: 'Hot chocolate',
+    category : 'Coffee',
+    rating : 4.3,
+    price: 249,
+    img: 'hot.jpg',
+    quantity: 1
+},
+{
+    id: 8,
+    name: 'Caramel macchiato',
+    category : 'Coffee',
+    rating : 4.3,
+    price: 199,
+    img: 'Caramel macchiato.jpg',
+    quantity: 1
+},
+{
+    id: 9,
+    name: 'Cortado',
+    category : 'Coffee',
+    rating : 4.3,
+    price: 99,
+    img: 'Cortado.jpg',
+    quantity: 1
+},
+{
+    id: 10,
+    name: 'Cold Brew Black',
+    category : 'coldbrew',
+    rating : 4.3,
+    price: 119,
+    img: 'Cold Brew Black.jpg',
+    quantity: 1
+},
+{
+    id: 11,
+    name: 'Vanilla Sweet',
+    category : 'coldbrew',
+    rating : 4.3,
+    price: 199,
+    img: 'Vanilla Sweet.jpg',
+    quantity: 1
+},
+{
+    id: 12,
+    name: 'Citrus cold brew',
+    category : 'coldbrew',
+    rating : 4.3,
+    price: 146,
+    img: 'Citrus cold brew.jpg',
+    quantity: 1
+},
+{
+    id: 13,
+    name: 'Vietnamese cold',
+    category : 'coldbrew',
+    rating : 4.3,
+    price: 129,
+    img: 'Vietnamese cold.jpg',
+    quantity: 1
+},
+{
+    id: 14,
+    name: 'Espresso brew',
+    category : 'coldbrew',
+    rating : 4.3,
+    price: 179,
+    img: 'Espresso brew.jpeg',
+    quantity: 1
+},
+{
+    id: 15,
+    name: 'Double brew',
+    category : 'coldbrew',
+    rating : 4.3,
+    price: 209,
+    img: 'Double brew.jpeg',
+    quantity: 1
+},
+{
+    id: 16,
+    name: 'Mocha',
+    category : 'frappuccino',
+    rating : 4.3,
+    price: 249,
+    img: 'Mocha.jpeg',
+    quantity: 1
+
+},
+{
+    id: 17,
+    name: 'Java Chip',
+    category : 'frappuccino',
+    rating : 4.3,
+    price: 339,
+    img: 'Java Chip.jpg',
+    quantity: 1
+},
+{
+    id: 18,
+    name: 'Espresso',
+    category : 'frappuccino',
+    rating : 4.3,
+    price: 189,
+    img: 'Espresso.jpg',
+    quantity: 1
+
+},
+{
+    id: 19,
+    name: 'Caramel java chip',
+    category : 'frappuccino',
+    rating : 4.3,
+    price: 199,
+    img: 'Caramel java chip.jpg',
+    quantity: 1
+
+},
+{
+    id: 20,
+    name: 'Chocolate Chip',
+    category : 'frappuccino',
+    rating : 4.3,
+    price: 225,
+    img: 'Chocolate Chip.jpeg',
+    quantity: 1
+},
+{
+    id: 21,
+    name: 'Double Chocolate',
+    category : 'frappuccino',
+    rating : 4.3,
+    price: 221,
+    img: 'Double Chocolate.jpeg',
+    quantity: 1
+},
+{
+    id: 22,
+    name: 'Creme Chip',
+    age: '32',
+    category : 'frappuccino',
+    rating : 4.3,
+    price: 149,
+    img: 'Creme Chip.jpeg',
+    quantity: 1
+},
+{
+    id: 23,
+    name: 'Pumpkin Spic',
+    category : 'Grandedrink',
+    rating : 4.3,
+    price: 119,
+    img: 'Pumpkin Spic.jpg',
+    quantity: 1
+},
+{
+    id: 24,
+    name: 'Iced Pumpkin',
+    category : 'Grandedrink',
+    rating : 4.3,
+    price: 144,
+    img: 'Iced Pumpkin.png',
+    quantity: 1
+},
+{
+    id: 25,
+    name: 'Apple Crisp',
+    category : 'Grandedrink',
+    rating : 4.3,
+    price: 149,
+    img: 'Apple Crisp.jpeg',
+    quantity: 1
+},
+{
+    id: 26,
+    name: 'Iced Apple',
+    category : 'Grandedrink',
+    rating : 4.3,
+    price: 179,
+    img: 'Iced Apple.jpg',
+    quantity: 1
+},
+{
+    id: 27,
+    name: 'Baked Apple',
+    category : 'Grandedrink',
+    rating : 4.3,
+    price: 79,
+    img: 'Baked Apple.jpeg',
+    quantity: 1
+},
+{
+    id: 28,
+    name: 'Pumpkin Cream',
+    category : 'Grandedrink',
+    rating : 4.3,
+    price: 99,
+    img: 'Pumpkin Cream.jpeg',
+    quantity: 1
+},
+{
+    id: 29,
+    name: 'Oatmilk Frappuc',
+    category : 'Grandedrink',
+    rating : 4.3,
+    price: 199,
+    img: 'Oatmilk Frappuccino.jpg',
+    quantity: 1
+},
+{
+    id: 30,
+    name: 'Choco Cake Pop',
+    category : 'bakery',
+    rating : 4.3,
+    price: 119,
+    img: 'Chocolate Cake Pop.jpg',
+    quantity: 1
+},
+{
+    id: 31,
+    name: 'Birthday Cake Pop',
+    category : 'bakery',
+    rating : 4.3,
+    price: 119,
+    img: 'Birthday Cake Pop.jpg',
+    quantity: 1
+},
+{
+    id: 32,
+    name: 'Coffee Cake',
+    category : 'bakery',
+    rating : 4.3,
+    price: 89,
+    img: 'Coffee Cake.jpg',
+    quantity: 1
+},
+{
+    id: 33,
+    name: 'Iced Lemon Loaf',
+    category : 'bakery',
+    rating : 4.3,
+    price: 169,
+    img: 'Iced Lemon Loaf.jpg',
+    quantity: 1
+},
+{
+    id: 34,
+    name: 'Cheese Danish',
+    category : 'bakery',
+    rating : 4.3,
+    price: 149,
+    img: 'Cheese Danish.jpg',
+    quantity: 1
+},
+{
+    id: 35,
+    name: 'Glazed Doughnut',
+    category : 'bakery',
+    rating : 4.3,
+    price: 109,
+    img: 'Glazed Doughnut.jpg',
+    quantity: 1
+},
+{
+    id: 36,
+    name: 'Blueberry muffin',
+    category : 'bakery',
+    rating : 4.3,
+    price: 99,
+    img: 'Blueberry muffin.jpg',
+    quantity: 1
+},
+{
+    id: 37,
+    name: 'Grilled Cheese',
+    category : 'lunch',
+    rating : 4.3,
+    price: 185,
+    img: 'Grilled Cheese.jpg',
+    quantity: 1
+},
+{
+    id: 38,
+    name: 'Ham & Swiss',
+    category : 'lunch',
+    rating : 4.3,
+    price: 201,
+    img: 'Ham & Swiss.jpg',
+    quantity: 1
+},
+{
+    id: 39,
+    name: 'Turkey provolone',
+    category : 'lunch',
+    rating : 4.3,
+    price: 129,
+    img: 'Turkey provolone.jpg',
+    quantity: 1
+},
+{
+    id: 40,
+    name: 'Tommata & Moz',
+    category : 'lunch',
+    rating : 4.3,
+    price: 100,
+    img: 'Tommata & Mozza.jpg',
+    quantity: 1
+},
+{
+    id: 41,
+    name: 'PB &j Protein BOX',
+    category : 'lunch',
+    rating : 4.3,
+    price: 159,
+    img: 'PB &j Protein BOX.jpg',
+    quantity: 1
+},
+{
+    id: 42,
+    name: 'Avocdo spread',
+    category : 'lunch',
+    rating : 4.3,
+    price: 140,
+    img: 'Avocdo spread.jpg',
+    quantity: 1
+},
+{
+    id: 43,
+    name: 'Cheese & fuit Box',
+    category : 'lunch',
+    rating : 4.3,
+    price: 259,
+    img: 'Cheese & fruits Box.jpg',
+    quantity: 1
+},
+{
+    id: 44,
+    name: 'Cheddar & Salami',
+    category : 'lunch',
+    rating : 4.3,
+    price: 149,
+    img: 'Cheddar & Salami.jpg',
+    quantity: 1
+},
+]
+
+export {foodItem};
